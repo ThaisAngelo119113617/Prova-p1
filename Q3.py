491f0e0f6690be23abcab3065c4a3b5b9ae5dd29
ThaisAngelo119113617 <56059840+ThaisAngelo119113617@users.noreply.github.com>
2019-10-05 16:56:14 -0300

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,10 +1,16 @@
 #QUESTA 3 P1
-soma = 0
-div = 1
-soma2termos = 1
-while( soma2termos > (5 * (10 ** (-8)))):
-    soma2termos = (1/div) - (1/(div + 2))
-    soma ++ abs(valor)
-    div += 4
-pi = 4 * soma
-print(pi)
+soma = 1
+pi = 0
+i = 1
+sinal = 1
+while (soma > (5 * (10 ** (-8)))):
+    pi1= pi
+    pi = pi + 4/(i * sinal)
+    i += 2
+    sinal  =sinal * (-1)
+    soma = (pi1 - pi) * sinal
+
+print('Soma = ' , soma)
+print('pi = ' , pi)
+
+
