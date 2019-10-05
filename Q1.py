e27f22445dd123f49a353fa19d46ad8698aa97f1
ThaisAngelo119113617 <56059840+ThaisAngelo119113617@users.noreply.github.com>
2019-10-05 17:04:52 -0300

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,16 +1,33 @@
 def raizes():
+    
     a = int(input('Entre com o valor do coeficiente a: '))
+    
     b = int(input('Entre com o valor do coeficiente b: '))
+    
     c = int(input('Entre com o valor do coeficiente c: '))
-    delta = int((b ** 2 ) - (4 * a * c))
+    
+    delta = ((b ** 2 ) - (4 * a * c))
+    
     print('Delta = ' , delta )
-    x1 = ((b ** 2 ) + ((delta )**(1/2)))/ (2 * a)
-    x2 = ((b ** 2 ) -((delta )**(1/2)))/ (2 * a)
-    if(delta >= 0):
+    
+    if( delta >= 0 ):
+ 
+        x1 = (( -b ) + ((delta )**(1/2)))/ (2 * a)
+        
+        x2 = (( -b ) -((delta )**(1/2)))/ (2 * a)
+        
         print('As raizes sao: ' , x1, '  e ' ,  x2)
-        print(' 0')
-    elif(delta < 0):
-        print('As raizes sao : ' , x1, ' e ', x2)
+        
+        print(' 0 ')
+    
+    elif( delta < 0 ):
+        
+        x1i = ((( -b ) + ((delta )**(1/2))) * i) / (2 * a) 
+
+        x2i = ((( -b ) - ((delta )**(1/2))) * i) / (2 * a)
+
+        print('As raizes sao : ' , x1i , ' e ', x2i )
+        
         print(' 1 ')
 
 
