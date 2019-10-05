975f278363b4b08c16f3b5703d5dd3076ca30505
ThaisAngelo119113617 <56059840+ThaisAngelo119113617@users.noreply.github.com>
2019-10-05 17:25:49 -0300

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,33 +1,40 @@
+#QUESTAO 1
 def raizes():
     
     a = int(input('Entre com o valor do coeficiente a: '))
-    
+
     b = int(input('Entre com o valor do coeficiente b: '))
-    
+
     c = int(input('Entre com o valor do coeficiente c: '))
-    
-    delta = ((b ** 2 ) - (4 * a * c))
-    
-    print('Delta = ' , delta )
-    
-    if( delta >= 0 ):
- 
-        x1 = (( -b ) + ((delta )**(1/2)))/ (2 * a)
-        
-        x2 = (( -b ) -((delta )**(1/2)))/ (2 * a)
-        
-        print('As raizes sao: ' , x1, '  e ' ,  x2)
-        
+
+    delta = ((b ** 2) - (4 * a * c))
+
+    print('Delta = ', delta)
+
+    if (delta >= 0):
+
+        x1 = ((-b) + ((delta) ** (1 / 2))) / (2 * a)
+
+        x2 = ((-b) - ((delta) ** (1 / 2))) / (2 * a)
+
+        print('As raizes sao: ', x1, '  e ', x2)
+
         print(' 0 ')
-    
-    elif( delta < 0 ):
-        
-        x1i = ((( -b ) + ((delta )**(1/2))) * i) / (2 * a) 
 
-        x2i = ((( -b ) - ((delta )**(1/2))) * i) / (2 * a)
+    elif (delta < 0):
 
-        print('As raizes sao : ' , x1i , ' e ', x2i )
-        
+        partereal = ( -b ) / 2* a
+
+        parteimaginariax1 = ((delta) ** (1 / 2)) / (2 * a)
+
+        parteimaginariax2 = - (((delta) ** (1 / 2))  / (2 * a))
+
+        print(' Parte real das raizes = ' , partereal)
+
+        print(' Parte imagnaria de X1 = ', parteimaginariax1)
+
+        print('Parte imaginaria de X2 = ' , parteimaginariax2)
+
         print(' 1 ')
 
 
